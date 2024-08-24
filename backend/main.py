019f7adce4d033b3d2a83b18d6dc61eaed1996e0
LeHuyTu <101401285+21522173huy@users.noreply.github.com>
2024-08-24 14:58:00 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -54,24 +54,39 @@ async def real2anime(file: UploadFile, option_json: str = Form(...)):
     options = json.loads(option_json)
 
     # Build the prompt based on options
-    prompt = "masterpiece, best_quality"
-    if options.get('gender'):
-        prompt += ", " + options['gender']
-    if options.get('hair'):
-        prompt += ", " + options['hair']
-    if options.get('accessories'):
-        prompt += ", " + ", ".join(options['accessories'])
-
+    prompt = ""
+    
+    if options.get('gender') is not None:
+        if options['gender'] == "male":
+          prompt += "1boy"
+        else : prompt += "1girl"
+    
+    if options.get('hair') is not None:
+        prompt += ", " + options['hair'] + " hair"
+    
+    prompt += ", looking at viewer"
+    
     list_prompt = []
-    if options.get('emote'):
+    if options.get('emote') is not None and len(options['emote']) != 0:
         for each in options['emote']:
             new_prompt = prompt + ", " + each
             list_prompt.append(new_prompt)
-    else:
-        list_prompt.append(prompt)
-
-    negative_prompt = 'nsfw, bad anatomy, worst quality'
-    strength = options.get('strength')
+    
+    else : list_prompt.append(prompt)
+    
+    for idx in range(len(list_prompt)):
+      list_prompt[idx] += ", upper body"
+    
+      if options.get('accessories') and len(options['accessories']) != 0:
+        list_prompt[idx] += ", " + ", ".join(options['accessories'])
+    
+      list_prompt[idx] += ", masterpiece"
+    
+      if options.get('age') is not None:
+          list_prompt[idx] += ", " + options['age'] + " year olds"
+    
+    negative_prompt = 'nsfw'
+    strength = options['strength']
 
     # Open the uploaded image
     image_data = await file.read()
